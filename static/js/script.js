b53bae16af03b184beb6be1c677a9f1a49e4febf
Jentek <oleksandr.aleksashyn@outlook.com>
2022-10-12 17:00:26 +0300

    Add edit form to cafe card

--- static/js/script.js
+++ static/js/script.js
@@ -9,8 +9,52 @@ cafes.forEach(cafe => {
   });
 
   //configure cancel btn
-  const cancelBtn = delConfirm.querySelector(".cancel");
-  cancelBtn.addEventListener("click", () => {
+  const cancelDel = delConfirm.querySelector(".cancel");
+  cancelDel.addEventListener("click", () => {
     delConfirm.classList.remove("display");
+  });
+
+  //configure edit form appearence
+  const editBtn = cafe.querySelector(".edit");
+  const editForm = cafe.querySelector(".edit-form");
+  editBtn.addEventListener("click", () => {
+    editForm.classList.add("display");
+  });
+
+  //configure cancel editing
+  const cancelEdit = editForm.querySelector(".cancel");
+  cancelEdit.addEventListener("click", () => {
+    editForm.classList.remove("display");
+  });
+
+  //configure edit form toggles
+  const toggles = editForm.querySelectorAll(".toggle");
+  toggles.forEach(toggle => {
+    const checkBtn = toggle.querySelector(".toggle-check");
+    const uncheckBtn = toggle.querySelector(".toggle-uncheck");
+    const checkbox = toggle.querySelector(".hidden-checkbox");
+
+    if (checkbox.checked) {
+      checkBtn.classList.add("checked");
+      uncheckBtn.classList.remove("checked");
+    } else {
+      checkBtn.classList.remove("checked");
+      uncheckBtn.classList.add("checked");
+    }
+
+    //add checking logic for buttons
+    checkBtn.addEventListener("click", e => {
+      e.preventDefault();
+      checkBtn.classList.add("checked");
+      uncheckBtn.classList.remove("checked");
+      checkbox.checked = true;
+    });
+
+    uncheckBtn.addEventListener("click", e => {
+      e.preventDefault();
+      checkBtn.classList.remove("checked");
+      uncheckBtn.classList.add("checked");
+      checkbox.checked = false;
+    });
   });
 });
