1db1ba5c81f42df458373fa159d7e07160725733
Jentek <oleksandr.aleksashyn@outlook.com>
2022-10-13 10:42:57 +0300

    Add edit-form data fetching.
    Added cafe/cafe_id endpoint to fetch cafe data via get request and display it in the edit-form on front-end.

--- main.py
+++ main.py
@@ -1,9 +1,10 @@
-from flask import Flask, render_template, url_for
+from flask import Flask, render_template, url_for, request, jsonify
 from sqlalchemy.orm import Session
 from db_schemas import engine, Cafe
 
 
 app = Flask(__name__)
+app.config["JSON_AS_ASCII"] = False
 
 
 @app.route("/")
@@ -11,6 +12,16 @@ def home():
     with Session(engine) as session:
         cafes = session.query(Cafe).all()
         return render_template("index.html", cafes=cafes)
+
+
+@app.route("/cafe/<int:cafe_id>", methods=["GET", "PATCH"])
+def cafe(cafe_id: int):
+    if request.method == "PATCH":
+        pass
+    else:
+        with Session(engine) as session:
+            requested_cafe = session.query(Cafe).where(Cafe.id == cafe_id).first()
+            return jsonify(cafe=requested_cafe.to_dict())
 
 
 if __name__ == "__main__":
--- static/js/script.js
+++ static/js/script.js
@@ -19,8 +19,41 @@ cafes.forEach(cafe => {
   //configure edit form appearence
   const editBtn = cafe.querySelector(".edit");
   const editForm = cafe.querySelector(".edit-form");
+  const toggles = editForm.querySelectorAll(".toggle");
+
   editBtn.addEventListener("click", e => {
     e.preventDefault();
+
+    //fetch data and display it on form
+    fetchCafeData(editForm.action).then(data => {
+      const cafe = data.cafe;
+      editForm.querySelector("input[name='name']").value = cafe.name;
+      editForm.querySelector("input[name='location']").value = cafe.location;
+      editForm.querySelector("input[name='location-url']").value = cafe.map_url;
+      editForm.querySelector("input[name='sockets']").checked = cafe.has_sockets;
+      editForm.querySelector("input[name='wifi']").checked = cafe.has_wifi;
+      editForm.querySelector("input[name='calls']").checked = cafe.can_take_calls;
+      editForm.querySelector("input[name='bathroom']").checked = cafe.has_toilet;
+      console.log(editForm.querySelector("input[name='bathroom']").checked);
+      console.log(cafe.has_toilet);
+      editForm.querySelector("input[name='coffee-price']").value = cafe.coffee_price;
+      editForm.querySelector("input[name='number-of-seats']").value = cafe.seats;
+
+      toggles.forEach(toggle => {
+        const checkBtn = toggle.querySelector(".toggle-check");
+        const uncheckBtn = toggle.querySelector(".toggle-uncheck");
+        const checkbox = toggle.querySelector(".hidden-checkbox");
+
+        if (checkbox.checked) {
+          checkBtn.classList.add("checked");
+          uncheckBtn.classList.remove("checked");
+        } else {
+          checkBtn.classList.remove("checked");
+          uncheckBtn.classList.add("checked");
+        }
+      });
+    });
+
     editForm.classList.add("display");
   });
 
@@ -31,22 +64,12 @@ cafes.forEach(cafe => {
     editForm.classList.remove("display");
   });
 
-  //configure edit form toggles
-  const toggles = editForm.querySelectorAll(".toggle");
+  ///add checking logic for toggle buttons
   toggles.forEach(toggle => {
     const checkBtn = toggle.querySelector(".toggle-check");
     const uncheckBtn = toggle.querySelector(".toggle-uncheck");
     const checkbox = toggle.querySelector(".hidden-checkbox");
 
-    if (checkbox.checked) {
-      checkBtn.classList.add("checked");
-      uncheckBtn.classList.remove("checked");
-    } else {
-      checkBtn.classList.remove("checked");
-      uncheckBtn.classList.add("checked");
-    }
-
-    //add checking logic for buttons
     checkBtn.addEventListener("click", e => {
       e.preventDefault();
       checkBtn.classList.add("checked");
@@ -62,3 +85,10 @@ cafes.forEach(cafe => {
     });
   });
 });
+
+async function fetchCafeData(url) {
+  const promise = await fetch(url);
+  const data = promise.json();
+
+  return data;
+}
